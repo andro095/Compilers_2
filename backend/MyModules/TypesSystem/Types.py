4799925870977171b8412db49083106a73892bc5
andro095 <andre.rodriguezgt@yahoo.com>
2022-08-26 01:40:53 -0600

    Sistema de tipos listo

--- backend/MyModules/TypesSystem/Types.py
+++ backend/MyModules/TypesSystem/Types.py
@@ -1,7 +1,7 @@
 import json
 from SymbolTable.Table import TableItem
 
-import TypesSystem
+from ConsoleMessages import MessagesDB
 from .Constants import types_sys_constants, typs, basic_types
 from SymbolTable import SymbolTable
 
@@ -14,6 +14,7 @@ def is_terminal_node(node) -> bool:
 
 class TypeSystem:
     def __init__(self):
+        self.msgs_db = MessagesDB()
         
         self.rules ={
             (typs.ID, typs.ASIGN): self.assign,
@@ -61,10 +62,19 @@ class TypeSystem:
                     elif ctx.children[1].symbol.type == typs.POINT or ctx.children[1].symbol.type == typs.ASIGN:
                         return elem.typ
                     else:
+                        line = (ctx.start.line, ctx.start.column)
+                        self.msgs_db.insert_errror(line, f'No se puede acceder a {ctx.children[1].getText()} de una variable')
                         return global_constants.ERROR_TYPE
                 elif elem.sem_kind == global_constants.METHOD:
-                    return elem.typ if ctx.getChildCount() > 1 and ctx.children[1].symbol.type == typs.LROUND else global_constants.ERROR_TYPE
+                    if ctx.getChildCount() > 1 and ctx.children[1].symbol.type == typs.LROUND:
+                        return elem.typ
+                    else: 
+                        line = (ctx.start.line, ctx.start.column)
+                        self.msgs_db.insert_error(line, f'No se puede acceder a {ctx.children[1].getText()} de un método')
+                        return global_constants.ERROR_TYPE
             else:
+                line = (ctx.start.line, ctx.start.column)
+                self.msgs_db.insert_error(line, f'{ctx.children[0].getText()} no existe')
                 return global_constants.ERROR_TYPE
         elif ctx.children[0].symbol.type == typs.INTEGER:
             return basic_types.INT
@@ -82,31 +92,39 @@ class TypeSystem:
         if any(map(lambda x: x == global_constants.ERROR_TYPE, types)):
             return global_constants.ERROR_TYPE
         
+        line = (ctx.start.line, ctx.start.column)
+        
         if self.sym_table.exists(ctx.children[0].getText()):
             feature_item = self.sym_table.get(ctx.children[0].getText())
             if feature_item.sem_kind == global_constants.ATTR:
                 if not self.sym_table.exists(ctx.children[2].getText()):
+                    self.msgs_db.insert_error(line, f'Tipo {ctx.children[2].getText()} no existente')
                     return global_constants.ERROR_TYPE
                 
                 if ctx.getChildCount() == 3:
                     return global_constants.CHECK_TYPE
                 
                 if types[4] != feature_item.typ and types[4] != basic_types.SELF_TYPE:
+                    self.msgs_db.insert_error(line, f'Tipo {types[4]} no es compatible con {feature_item.typ}')
                     return global_constants.ERROR_TYPE
                 else:
                     return global_constants.CHECK_TYPE
             elif feature_item.sem_kind == global_constants.METHOD:
                 if ctx.children[-4].getText() != basic_types.SELF_TYPE and not self.sym_table.exists(ctx.children[-4].getText()):
+                    self.msgs_db.insert_error(line, f'Tipo {ctx.children[-4].getText()} no existente')
                     return global_constants.ERROR_TYPE
                 
                 if types[-2] == None:
+                    self.msgs_db.insert_error(line, f'Sin tipo')
                     return global_constants.ERROR_TYPE
                 
                 if types[-2] != feature_item.typ and types[-2] != global_constants.NO_TYPE:
+                    self.msgs_db.insert_error(line, f'Tipo {types[-2]} no es compatible con {feature_item.typ}')
                     return global_constants.ERROR_TYPE
                 else:
                     return global_constants.CHECK_TYPE
         else:
+            self.msgs_db.insert_error(line, f'{ctx.children[0].getText()} no existe')
             return global_constants.ERROR_TYPE
         
     def validate_expr(self, ctx: YaplParser.ExprContext, types: list[str] ,res: list[bool]) -> str:
@@ -116,17 +134,26 @@ class TypeSystem:
         if self.sym_table.exists(ctx.children[0].getText()) and self.sym_table.exists(ctx.children[2].getText()):
             return global_constants.CHECK_TYPE
         else:
+            line = (ctx.start.line, ctx.start.column)
+            self.msgs_db.insert_error(line, f'Paraetro {ctx.children[0].getText()} y/o tipo {ctx.children[2].getText()} no delcarado')
             return global_constants.ERROR_TYPE
         
     def lround_expr(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
         return types[1]
     
     def is_void(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
-        return basic_types.BOOL if types[1] != global_constants.ERROR_TYPE else global_constants.ERROR_TYPE
+        if types[1] != global_constants.ERROR_TYPE:
+            return basic_types.BOOL
+        else:
+            line = (ctx.start.line, ctx.start.column)
+            self.msgs_db.insert_error(line, f'{ctx.children[1].getText()} no es compatible con {types[1]}')
+            return global_constants.ERROR_TYPE
     
     def compare(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:        
         if types[0] == global_constants.ERROR_TYPE or types[2] == global_constants.ERROR_TYPE:
             return global_constants.ERROR_TYPE
+        
+        line = (ctx.start.line, ctx.start.column)
         
         if self.sym_table.exists(types[0]) and self.sym_table.exists(types[2]):
             if types[0] == types[2]:
@@ -138,22 +165,42 @@ class TypeSystem:
                 if father0 == father2 or types[0] == father2 or types[2] == father0:
                     return basic_types.BOOL
                 else:
+                    self.msgs_db.insert_error(line, f'{types[0]} no es compatible con {types[2]}')
                     return global_constants.ERROR_TYPE
         else: 
+            self.msgs_db.insert_error(line, f'{types[0]} o {types[2]} no existe')
             return global_constants.ERROR_TYPE
             
     def not_expr(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
-        return basic_types.BOOL if self.cast_bool(types[1]) == basic_types.BOOL else global_constants.ERROR_TYPE
+        if self.cast_bool(types[1]) == basic_types.BOOL:
+            return basic_types.BOOL
+        else:
+            line = (ctx.start.line, ctx.start.column)
+            self.msgs_db.insert_error(line, f'{types[1]} no es compatible con {basic_types.BOOL}')
+            return global_constants.ERROR_TYPE
     
     def arimetic(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
-        return basic_types.INT if self.cast_int(types[0]) == basic_types.INT and self.cast_int(types[2]) == basic_types.INT else global_constants.ERROR_TYPE
+        if self.cast_int(types[0]) == basic_types.INT and self.cast_int(types[2]) == basic_types.INT:
+            return basic_types.INT
+        else:
+            line = (ctx.start.line, ctx.start.column)
+            self.msgs_db.insert_error(line, f'{types[0]} o {types[2]} no son enteros')
+            return global_constants.ERROR_TYPE
     
     def not_int(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
-        return basic_types.INT if self.cast_int(types[1]) == basic_types.INT else global_constants.ERROR_TYPE
+        if self.cast_int(types[1]) == basic_types.INT:
+            return basic_types.INT
+        else:
+            line = (ctx.start.line, ctx.start.column)
+            self.msgs_db.insert_error(line, f'{types[1]} no es compatible con {basic_types.INT}')
+            return global_constants.ERROR_TYPE
+            
     
     def assign(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
         if types[0] == global_constants.ERROR_TYPE or types[2] == global_constants.ERROR_TYPE:
             return global_constants.ERROR_TYPE
+    
+        line = (ctx.start.line, ctx.start.column)
         
         if self.sym_table.exists(ctx.children[0].getText()) and self.sym_table.exists(types[2]):    
             id_item: TableItem = self.sym_table.get(ctx.children[0].getText())
@@ -166,23 +213,34 @@ class TypeSystem:
                 if id_item.typ == father2:
                     return types[2]
                 else:           
+                    self.msgs_db.insert_error(line, f'{id_item.typ} no es compatible con {types[2]}')
                     return global_constants.ERROR_TYPE
         else:
+            self.msgs_db.insert_error(line, f'{ctx.children[0].getText()} o {types[2]} no existe')
             return global_constants.ERROR_TYPE
         
     def block(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
         for i in range(1, len(types) - 1, 2):
             if types[i] == global_constants.ERROR_TYPE:
+                line = (ctx.start.line, ctx.start.column)
+                self.msgs_db.insert_error(line, 'Ocurre un error en el bloque')
                 return global_constants.ERROR_TYPE
         
         return types[-3]    
     
     def while_expr(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
+        line = (ctx.start.line, ctx.start.column)
+        
         if self.cast_bool(types[1]) != basic_types.BOOL:
+            self.msgs_db.insert_error(line, f'{types[1]} no es compatible con {basic_types.BOOL}')
             return global_constants.ERROR_TYPE
         
-        return basic_types.OBJECT if self.cast_bool(types[3]) != global_constants.ERROR_TYPE else global_constants.ERROR_TYPE        
-    
+        if self.cast_bool(types[3]) != global_constants.ERROR_TYPE:
+            return basic_types.OBJECT
+        else:
+            self.msgs_db.insert_error(line, 'Ocurre un error en el bloque')
+            return global_constants.ERROR_TYPE
+            
     def if_expr(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
         if any(map(lambda x: x == global_constants.ERROR_TYPE, types)) or self.cast_bool(types[1]) != basic_types.BOOL:
             return global_constants.ERROR_TYPE
@@ -193,6 +251,8 @@ class TypeSystem:
         if any(map(lambda x: x == global_constants.ERROR_TYPE, types)):
             return global_constants.ERROR_TYPE
         
+        line = (ctx.start.line, ctx.start.column) 
+        
         i = 1
         
         actual_type = None
@@ -200,11 +260,13 @@ class TypeSystem:
         while ctx.children[i].getText().lower() != types_sys_constants.IN:
             if isinstance(ctx.children[i], YaplParser.ExprContext):
                 if types[i] != actual_type:
+                    self.msgs_db.insert_error(line, f'{types[i]} no es compatible con {actual_type}')
                     return global_constants.ERROR_TYPE
             elif ctx.children[i].symbol.type == typs.ID:
                 if self.sym_table.exists(ctx.children[i].getText()) and self.sym_table.exists(ctx.children[i + 2].getText()):
                     actual_type = self.sym_table.get(ctx.children[i].getText()).typ
                 else:
+                    self.msgs_db.insert_error(line, f'{ctx.children[i].getText()} y/o {ctx.children[i + 2].getText()} no existe(n)')
                     return global_constants.ERROR_TYPE
                 
             i += 1
@@ -216,6 +278,8 @@ class TypeSystem:
         print(ctx.getText(), types, ctx.children[2].getText())
         if any(map(lambda x: x == global_constants.ERROR_TYPE, types)):
             return global_constants.ERROR_TYPE
+        
+        line = (ctx.start.line, ctx.start.column)
         
         if self.sym_table.exists(ctx.children[0].getText()):
             elem: TableItem = self.sym_table.get(ctx.children[0].getText())
@@ -232,7 +296,7 @@ class TypeSystem:
                 param_count = comma_count + 1
             
             if param_count != elem.param_num:
-                print(param_count, elem.param_num)
+                self.msgs_db.insert_error(line, f'El numero de parametros no coincide con el numero de parametros del método')
                 return global_constants.ERROR_TYPE
             
             table = self.sym_table.tables[self.sym_table.get_table_index(elem.lex)]
@@ -241,17 +305,21 @@ class TypeSystem:
             
             for i in range(elem.param_num):
                 if table.items[i].typ != types[indx]:
+                    self.msgs_db.insert_error(line, f'{table.items[i].typ} no es compatible con {types[indx]}')
                     return global_constants.ERROR_TYPE
                 else:
                     indx += 2
             
             return elem.typ
         else:
+            self.msgs_db.insert_error(line, f'{ctx.children[0].getText()} no existe')
             return global_constants.ERROR_TYPE
         
     def expr_point(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
         if any(map(lambda x: x == global_constants.ERROR_TYPE, types)):
             return global_constants.ERROR_TYPE
+        
+        line = (ctx.start.line, ctx.start.column)
         
         if self.sym_table.exists(types[0]):
             table_index = self.sym_table.get_table_index(types[0])
@@ -273,6 +341,7 @@ class TypeSystem:
                     
                 
                 if param_count != elem.param_num:
+                    self.msgs_db.insert_error(line, f'El numero de parametros no coincide con el numero de parametros del método')
                     return global_constants.ERROR_TYPE
                 
                 table = self.sym_table.tables[self.sym_table.get_table_index(elem.lex)]
@@ -281,19 +350,24 @@ class TypeSystem:
                 
                 for i in range(elem.param_num):
                     if table.items[i].typ != types[indx]:
+                        self.msgs_db.insert_error(line, f'{table.items[i].typ} no es compatible con {types[indx]}')
                         return global_constants.ERROR_TYPE
                     else:
                         indx += 2
                 
                 return elem.typ
             else:
+                self.msgs_db.insert_error(line, f'{ctx.children[2].getText()} no existe')
                 return global_constants.ERROR_TYPE
         else:
+            self.msgs_db.insert_error(line, f'{types[0]} no existe')
             return global_constants.ERROR_TYPE
         
     def expr_arroba(self, ctx: YaplParser.ExprContext, types: list[str]) -> str:
         if any(map(lambda x: x == global_constants.ERROR_TYPE, types)):
             return global_constants.ERROR_TYPE
+        
+        line = (ctx.start.line, ctx.start.column)
         
         if self.sym_table.exists(types[0]) and self.sym_table.exists(ctx.children[2].getText()):
             table_index = self.sym_table.get_table_index(types[0])
@@ -314,6 +388,7 @@ class TypeSystem:
                     param_count = comma_count + 1
                 
                 if param_count != elem.param_num:
+                    self.msgs_db.insert_error(line, f'El numero de parametros no coincide con el numero de parametros del método')
                     return global_constants.ERROR_TYPE
                 
                 table = self.sym_table.tables[self.sym_table.get_table_index(elem.lex)]
@@ -322,15 +397,17 @@ class TypeSystem:
                 
                 for i in range(elem.param_num):
                     if table.items[i].typ != types[indx]:
+                        self.msgs_db.insert_error(line, f'{table.items[i].typ} no es compatible con {types[indx]}')
                         return global_constants.ERROR_TYPE
                     else:
                         indx += 2
                 
                 return elem.typ
             else:
+                self.msgs_db.insert_error(line, f'{ctx.children[4].getText()} no existe')
                 return global_constants.ERROR_TYPE
-            
         else: 
+            self.msgs_db.insert_error(line, f'{types[0]} o {ctx.children[2].getText()} no existe')
             return global_constants.ERROR_TYPE
             
         
--- backend/MyModules/YAPL/YaplSysTypeVisitor.py
+++ backend/MyModules/YAPL/YaplSysTypeVisitor.py
@@ -51,7 +51,7 @@ class YaplSysTypeVisitor(ParseTreeVisitor):
         res = evaluate_terminal_children(ctx.children)
         
         typs = self.visit_children(ctx, res)
-        self.msg_db.insert_message("Programa: " + str(typs))
+        # self.msg_db.insert_message("Programa: " + str(typs))
         
         if some_error_type(typs):
                 line = (ctx.start.line, ctx.start.column)
@@ -63,6 +63,9 @@ class YaplSysTypeVisitor(ParseTreeVisitor):
     
     # Visit a parse tree produced by YaplParser#class.
     def visitClass(self, ctx:YaplParser.ClassContext): 
+        if self.msg_db.error_flag:
+            return global_constants.ERROR_TYPE
+        
         res = evaluate_terminal_children(ctx.children)  
         
         class_exists = self.symbol_table.exists(ctx.children[1].getText())
@@ -70,7 +73,7 @@ class YaplSysTypeVisitor(ParseTreeVisitor):
         if class_exists:
             self.symbol_table.enter_scope(ctx.children[1].getText())
             typs = self.visit_children(ctx, res)
-            self.msg_db.insert_message(f"Clase {ctx.children[1].getText()}: " + str(typs))
+            # self.msg_db.insert_message(f"Clase {ctx.children[1].getText()}: " + str(typs))
             
             if some_error_type(typs):
                 line = (ctx.start.line, ctx.start.column)
@@ -88,6 +91,8 @@ class YaplSysTypeVisitor(ParseTreeVisitor):
     
     # Visit a parse tree produced by YaplParser#feature.
     def visitFeature(self, ctx:YaplParser.FeatureContext):
+        if self.msg_db.error_flag:
+            return global_constants.ERROR_TYPE
 
         res = evaluate_terminal_children(ctx.children)
         
@@ -102,7 +107,7 @@ class YaplSysTypeVisitor(ParseTreeVisitor):
                 self.symbol_table.enter_scope(ctx.children[0].getText())
                 
             typs = self.visit_children(ctx, res)
-            self.msg_db.insert_message(f"Feature {ctx.children[0].getText()}: " + str(typs))
+            # self.msg_db.insert_message(f"Feature {ctx.children[0].getText()}: " + str(typs))
             
             if some_error_type(typs):
                 line = (ctx.start.line, ctx.start.column)
@@ -123,35 +128,40 @@ class YaplSysTypeVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by YaplParser#formal.
     def visitFormal(self, ctx:YaplParser.FormalContext):
-        res = evaluate_terminal_children(ctx.children)     
+        if self.msg_db.error_flag:
+            return global_constants.ERROR_TYPE
         
-        self.msg_db.insert_message(f"Formal {ctx.children[0].getText()}: " + str(res))
+        # res = evaluate_terminal_children(ctx.children)     
+        
+        # self.msg_db.insert_message(f"Formal {ctx.children[0].getText()}: " + str(res))
         
         return types_sys.validate_formal(ctx)
     
     # Visit a parse tree produced by YaplParser#expr.
     def visitExpr(self, ctx:YaplParser.ExprContext):  
+        if self.msg_db.error_flag:
+            return global_constants.ERROR_TYPE
+        
         res = evaluate_terminal_children(ctx.children) 
                 
         typs = []
         
         if all(res):
             typs = self.get_type(ctx)
-            self.msg_db.insert_message(f"Expr {ctx.getText()}: " + str(typs))
+            # self.msg_db.insert_message(f"Expr {ctx.getText()}: " + str(typs))
             return typs
         else:
             if ctx.children[0].getText().lower() == constants.LET:
                 let_exists = self.symbol_table.exists(constants.LET + str(self.analized_lets))
                 if let_exists:
                     self.symbol_table.enter_scope(constants.LET + str(self.analized_lets))
-                    self.msg_db.insert_message(f'Scope Actual: {self.symbol_table.tables[self.symbol_table.actual_scope].name}')
                 else:
                     line = (ctx.start.line, ctx.start.column)
                     self.msg_db.insert_error(line, f'La let no existe.')
                     return global_constants.ERROR_TYPE
                 
             typs = self.visit_children(ctx, res)
-            self.msg_db.insert_message(f"Expr {ctx.getText()}: " + str(typs))
+            # self.msg_db.insert_message(f"Expr {ctx.getText()}: " + str(typs))
             
             if some_error_type(typs):
                 line = (ctx.start.line, ctx.start.column)
@@ -165,27 +175,6 @@ class YaplSysTypeVisitor(ParseTreeVisitor):
                 self.analized_lets += 1
                 
             return validation
-        
-        # self.msg_db.insert_message(f"Expr {ctx.children[0].getText()}: " + str(res))
-            
-        # TODO: función para obtener tipos de los nodos
-        # print(typs)
-        
-        
-        types = ['Int', 'Bool', global_constants.ERROR_TYPE]
-        
-        
-        # TODO: Obtener Regla a aplicar
-        
-        # TODO: Validar Regla
-        
-        # TODO: Retornar el tipo de la validación    
-                   
-        # has_children = ctx.getChildCount() > 0
-        # children = list(ctx.getChildren())
-        # self.insert_log(f'Entrando a la expresión {ctx.getText()} y el estado de su children es {has_children} y su children es {children}.', 'pink')
-        # self.visitChildren(ctx)
-        # self.insert_log(f'Saliendo de la expresión {ctx.getText()} y el estado de su children es {has_children} y su children es {children}.', 'pink')
 
 
 del YaplParser
